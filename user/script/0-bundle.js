d9f5c19bb36a6df68feec36fd2830f335aede58b
Jenkins <noreply@mapcreator.eu>
2017-07-13 14:26:11 +0200

    Update auto generated docs

--- user/script/0-bundle.js
+++ user/script/0-bundle.js
@@ -31,10 +31,10 @@
  * 
  */
 /*!
- * hash:239543bbdb9c2ca3e948, chunkhash:a19e825c34fe264924cf, name:bundle, version:v0.5.6
+ * hash:2dc752714df8a8be4ebc, chunkhash:6c086f5df6ab6b79e77c, name:bundle, version:v0.5.7
  * 
  * This budle contains the following packages:
- * └─ @mapcreator/maps4news (0.5.6) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
+ * └─ @mapcreator/maps4news (0.5.7) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
  *    ├─ babel-polyfill (6.23.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-polyfill/package.json
  *    │  ├─ babel-runtime (6.23.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-runtime/package.json
  *    │  │  └─ regenerator-runtime (0.10.5) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/regenerator-runtime/package.json
@@ -7387,7 +7387,7 @@ var ResourceCache = function () {
 
       (storage[page.cacheToken] || (storage[page.cacheToken] = [])).push(data);
 
-      this.emitter.emit('push', { page: page, validThrough: validThrough });
+      this.emitter.emit('push', { page: page, validThrough: validThrough, resourceUrl: page.url });
       this.emitter.emit('invalidate', { resourceUrl: page.url });
     }
 
@@ -9848,7 +9848,7 @@ exports.resources = _resources;
  * @private
  */
 
-var version = exports.version = "v0.5.6";
+var version = exports.version = "v0.5.7";
 
 /***/ }),
 /* 167 */
@@ -10008,6 +10008,8 @@ var PaginatedResourceWrapper = function () {
       this.data = this.cache.resolve(this.path, this._last.cacheToken).filter(function (value) {
         return typeof value !== 'undefined';
       });
+
+      this.cache.emitter.emit('post-rebuild', { resourceUrl: this._last.url });
     }
 
     /**
