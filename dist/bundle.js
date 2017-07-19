c4886433ad303b6b06c6a97542b2376cf6a2be28
Jenkins <noreply@mapcreator.eu>
2017-07-19 16:56:04 +0200

    Update auto generated docs

--- dist/bundle.js
+++ dist/bundle.js
@@ -31,10 +31,10 @@
  * 
  */
 /*!
- * hash:3ee4bce7f2bd2c5c4a27, chunkhash:24b27c314daf3b41465b, name:bundle, version:v0.5.10
+ * hash:6a6238a37265a3897ebd, chunkhash:8f1605874b4a84d5399a, name:bundle, version:v0.5.12
  * 
  * This budle contains the following packages:
- * └─ @mapcreator/maps4news (0.5.10) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
+ * └─ @mapcreator/maps4news (0.5.12) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
  *    ├─ babel-polyfill (6.23.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-polyfill/package.json
  *    │  ├─ babel-runtime (6.23.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-runtime/package.json
  *    │  │  └─ regenerator-runtime (0.10.5) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/regenerator-runtime/package.json
@@ -9943,7 +9943,7 @@ exports.resources = _resources;
  * @private
  */
 
-var version = exports.version = "v0.5.10";
+var version = exports.version = "v0.5.12";
 
 /***/ }),
 /* 167 */
