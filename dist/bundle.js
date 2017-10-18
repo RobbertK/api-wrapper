c6054de9676c1e1dd942e4934cd38603ea362f41
Jenkins <noreply@mapcreator.eu>
2017-10-18 16:11:34 +0200

    Update auto generated docs

--- dist/bundle.js
+++ dist/bundle.js
@@ -31,10 +31,10 @@
  * 
  */
 /*!
- * hash:630c1726dc74c3f3a016, chunkhash:a0a00ce774a400676d37, name:bundle, version:v1.1.36
+ * hash:bbd81a0b412bfadf21bf, chunkhash:77e7d681cd72ab4b54c3, name:bundle, version:v1.1.37
  * 
  * This bundle contains the following packages:
- * └─ @mapcreator/maps4news (1.1.36) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
+ * └─ @mapcreator/maps4news (1.1.37) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
  *    ├─ babel-polyfill (6.26.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-polyfill/package.json
  *    │  ├─ babel-runtime (6.26.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-runtime/package.json
  *    │  │  ├─ core-js (2.5.1) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/core-js/package.json
@@ -13780,7 +13780,7 @@ exports.errors = _errors;
  * @private
  */
 
-var version = exports.version = "v1.1.36";
+var version = exports.version = "v1.1.37";
 
 /**
  * Package license
