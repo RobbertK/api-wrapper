148012ba99bfc84e43379ec307702315b185d748
Jenkins <noreply@mapcreator.eu>
2017-11-01 12:29:44 +0100

    Update auto generated docs

--- dist/bundle.js
+++ dist/bundle.js
@@ -31,10 +31,10 @@
  * 
  */
 /*!
- * hash:41db0e604297f7457874, chunkhash:3c2995c58e579a996290, name:bundle, version:dea048f
+ * hash:8155437e6ae6cb122c40, chunkhash:0ef48585c918fb872251, name:bundle, version:v1.1.55
  * 
  * This bundle contains the following packages:
- * └─ @mapcreator/maps4news (1.1.53) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
+ * └─ @mapcreator/maps4news (1.1.55) ── BSD 3-clause "New" or "Revised" License (http://www.opensource.org/licenses/BSD-3-Clause) ── package.json
  *    ├─ babel-polyfill (6.26.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-polyfill/package.json
  *    │  ├─ babel-runtime (6.26.0) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/babel-runtime/package.json
  *    │  │  ├─ core-js (2.5.1) ── MIT License (http://www.opensource.org/licenses/MIT) ── node_modules/core-js/package.json
@@ -14185,7 +14185,7 @@ exports.errors = _errors;
  * @private
  */
 
-var version = exports.version = "dea048f";
+var version = exports.version = "v1.1.55";
 
 /**
  * Package license
@@ -24683,7 +24683,7 @@ var PasswordFlow = function (_OAuth) {
 
       var url = this.host + this._path;
       var query = {
-        'grant_type': '_password',
+        'grant_type': 'password',
         'client_id': this.clientId,
         'client_secret': this._secret,
         'username': this._username,
